ffc5f2d4d724f98d9eb111e0db5c1c0f481d888a
andreev-sergej <andreevsergej1111@gmail.com>
2025-02-17 20:40:49 +0300

    Add NM sigma estimation algorithms. Update Registry

--- src/algorithms/__init__.py
+++ src/algorithms/__init__.py
@@ -2,6 +2,12 @@ from src.algorithms.semiparam_algorithms.nvm_semi_param_algorithms.g_estimation_
     SemiParametricGEstimationGivenMu,
 )
 from src.algorithms.semiparam_algorithms.nvm_semi_param_algorithms.mu_estimation import SemiParametricMuEstimation
+from src.algorithms.semiparam_algorithms.nm_semi_param_algorithms.sigma_estimation_eigenvalue_based import (
+    SemiParametricMeanSigmaEstimationEigenvalueBased,
+)
+from src.algorithms.semiparam_algorithms.nm_semi_param_algorithms.sigma_estimation_empirical import (
+    SemiParametricMeanSigmaEstimationEmpirical,
+)
 from src.register.algorithm_purpose import AlgorithmPurpose
 from src.register.register import Registry
 
@@ -9,4 +15,10 @@ ALGORITHM_REGISTRY: Registry = Registry()
 ALGORITHM_REGISTRY.register("mu_estimation", AlgorithmPurpose.NMV_SEMIPARAMETRIC)(SemiParametricMuEstimation)
 ALGORITHM_REGISTRY.register("g_estimation_given_mu", AlgorithmPurpose.NMV_SEMIPARAMETRIC)(
     SemiParametricGEstimationGivenMu
+)
+ALGORITHM_REGISTRY.register("sigma_estimation_eigenvalue_based", AlgorithmPurpose.NM_SEMIPARAMETRIC)(
+    SemiParametricMeanSigmaEstimationEigenvalueBased
+)
+ALGORITHM_REGISTRY.register("sigma_estimation_empirical", AlgorithmPurpose.NM_SEMIPARAMETRIC)(
+    SemiParametricMeanSigmaEstimationEmpirical
 )
---
+++ src/algorithms/semiparam_algorithms/nm_semi_param_algorithms/sigma_estimation_empirical.py
@@ -0,0 +1,66 @@
+import math
+from typing import Optional, TypedDict
+
+import numpy as np
+
+from src.estimators.estimate_result import EstimateResult
+
+T_DEFAULT_VALUE = 7.5
+PARAMETER_KEYS = ["t"]
+
+
+class SemiParametricMeanSigmaEstimationEmpirical:
+    """Estimation of sigma parameter of NM mixture represented in canonical form Y = xi + sigma*N.
+
+    Args:
+        sample (Optional[np.ndarray]): Sample of the analyzed distribution.
+        params (Optional[dict]): Parameters of the algorithm.
+    """
+
+    class ParamsAnnotation(TypedDict, total=False):
+        t: float
+
+    def __init__(
+        self,
+        sample: Optional[np.ndarray] = None,
+        **kwargs: ParamsAnnotation,
+    ):
+        self.sample: np.ndarray = np.array([]) if sample is None else sample
+        self.n: int = len(self.sample)
+        self.t: float = self._validate_kwargs(**kwargs)
+
+    @staticmethod
+    def _validate_kwargs(**kwargs: ParamsAnnotation) -> float:
+        """Validate and extract parameters.
+
+        Args:
+            kwargs (dict): Parameters of the algorithm.
+
+        Returns:
+            float: Validated parameter `t`.
+        """
+        if any(key not in PARAMETER_KEYS for key in kwargs):
+            raise ValueError("Got unexpected parameter.")
+
+        t_value = kwargs.get("t", T_DEFAULT_VALUE)
+        if not isinstance(t_value, (float, int)):
+            raise ValueError("Expected a numeric value (float or int) as parameter `t`.")
+
+        t = float(t_value)
+        if t <= 0:
+            raise ValueError("Expected a positive float as parameter `t`.")
+        return t
+
+    def algorithm(self, sample: np.ndarray) -> EstimateResult:
+        """Estimate the sigma parameter.
+
+        Args:
+            sample (np.ndarray): Sample of the analyzed distribution.
+
+        Returns:
+            EstimateResult: An object containing the estimated sigma value.
+        """
+        if sample.size == 0:
+            raise ValueError("Sample can't be empty.")
+        sigma = ((2 / (self.t**2)) * math.log((1 / self.n) * sum([math.exp(self.t * x) for x in self.sample]))) ** 0.5
+        return EstimateResult(value=sigma, success=True)

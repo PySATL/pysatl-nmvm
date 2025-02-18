73304b3d888582d6cb768ba4dbb0a2aca85dfdbe
andreev-sergej <andreevsergej1111@gmail.com>
2025-02-18 14:55:29 +0300

    Add NVM Semiparametric RQMC-Based Mixing Density Estimation Algorithm

--- src/algorithms/__init__.py
+++ src/algorithms/__init__.py
@@ -2,6 +2,9 @@ from src.algorithms.semiparam_algorithms.nvm_semi_param_algorithms.g_estimation_
     SemiParametricGEstimationGivenMu,
 )
 from src.algorithms.semiparam_algorithms.nvm_semi_param_algorithms.mu_estimation import SemiParametricMuEstimation
+from src.algorithms.semiparam_algorithms.nvm_semi_param_algorithms.g_estimation_given_mu_rqmc_based import (
+    SemiParametricGEstimationGivenMuRQMCBased,
+)
 from src.register.algorithm_purpose import AlgorithmPurpose
 from src.register.register import Registry
 
@@ -9,4 +12,7 @@ ALGORITHM_REGISTRY: Registry = Registry()
 ALGORITHM_REGISTRY.register("mu_estimation", AlgorithmPurpose.NMV_SEMIPARAMETRIC)(SemiParametricMuEstimation)
 ALGORITHM_REGISTRY.register("g_estimation_given_mu", AlgorithmPurpose.NMV_SEMIPARAMETRIC)(
     SemiParametricGEstimationGivenMu
+)
+ALGORITHM_REGISTRY.register("g_estimation_given_mu_rqmc_based", AlgorithmPurpose.NMV_SEMIPARAMETRIC)(
+    SemiParametricGEstimationGivenMuRQMCBased
 )
